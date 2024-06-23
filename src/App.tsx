03a666fa8f3050459acdb0a2bf12ced8b0f8f3bc
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-06-23 17:34:47 +0700

    Test my app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Saya sedang melakukan pengembangan website</p>
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/GoogleFirebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCbPUXmm56uhv0OivDeX0hWK0RuqY110UE",
+  authDomain: "repo-turgo.firebaseapp.com",
+  projectId: "repo-turgo",
+  storageBucket: "repo-turgo.appspot.com",
+  messagingSenderId: "1012935452535",
+  appId: "1:1012935452535:web:cb1ff81071494d3228e372",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
