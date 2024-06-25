92fceba17074a99fcbcd052497cafddb500eb8de
Mukhamad Khusaini <khusainimukhamad@gmail.com>
2024-06-25 18:56:15 +0700

    Add template undangan component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Topper from "./components/Topper";
+import Template_undangan from "./components/Template-undangan";
 
 function App() {
   return (
     <main>
       <Topper />
+      <div className="vertical-indenting"></div>
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
+      <Template_undangan />
     </main>
   );
 }
--- src/components/template-undangan.tsx
+++ src/components/template-undangan.tsx
@@ -1,3 +1,16 @@
+import style from "../assets/css/template-undangan.module.css";
+
 export default function Template_undangan() {
-  return <></>;
+  return (
+    <div className={style["template-undangan"]}>
+      <div className={style["template-single"]}>
+        <img src="/images/undangan.png" alt="undangan" />
+        <div>
+          <h4>Template undangan layatan</h4>
+          <hr />
+          <p>Download template undangan layatan</p>
+        </div>
+      </div>
+    </div>
+  );
 }
